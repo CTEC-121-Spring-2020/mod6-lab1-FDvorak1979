6d9645eea4c801e67394af09e0810615eaa782bb
FDvorak1979 <f.dvorak2@students.clark.edu>
2020-05-27 12:19:20 -0700

    done

--- Template.py
+++ Template.py
@@ -1,8 +1,8 @@
 """
 CTEC 121
-<your name>
-<assignment/lab name>
-<assignment/lab description
+Frank Dvorak
+Module 6 Lab 1
+
 """
 
 """ IPO template
@@ -12,6 +12,36 @@ Output: return value and description
 """
 
 def main():
-    # code here
+   '''
+    # definite loop example
+    for i in range(0, 11, 2):
+        print(i)
+    print()
+    #same behaviour but coded as indefinite while loop
+    count = 0
+    while(count < 11):
+        print(count)
+        #count = count + 2
+        count += 2
 
+    x = True
+    y = True
+    if(x):
+        if(y):
+            print("both true")
+        else:
+            print("y false")
+
+    if x and y:
+        print("both true")
+    else:
+        print("other")
+
+    print()
+    '''
+    print(True and False)
+    print(True and True)
+    print(True or False)
+    print(False and False)
+    
 main()    
